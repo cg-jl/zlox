734a5661dd856d8e42b97372157ce1ddebd4398a
Jesús "gsus" Lapastora <cyber.gsuscode@gmail.com>
2023-04-05 16:24:11 +0200

    context: Make state public

--- src/context.zig
+++ src/context.zig
@@ -1,16 +1,16 @@
 const std = @import("std");
 
-var has_errored: bool = false;
+pub var has_errored: bool = false;
 
 fn setErrored() void {
     has_errored = true;
 }
 
-fn clear() void {
+pub fn clear() void {
     has_errored = false;
 }
 
-pub fn report(line: u32, comptime where: []const u8, comptime message: []const u8) void {
+pub fn report(line: u32, where: []const u8, comptime message: []const u8) void {
     std.log.err("[line {}] Error{s}: {s}", .{ line, where, message });
     setErrored();
 }
