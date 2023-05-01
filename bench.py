5bca47705b9aa69045a640f3df4c05785a43fda9
Jesús "gsus" Lapastora <cyber.gsuscode@gmail.com>
2023-05-01 03:17:09 +0200

    bench: Allow customization for count (time) for fib.lox

--- bench.py
+++ bench.py
@@ -1,14 +1,39 @@
 import subprocess
 import numpy as np
+import sys
 from multiprocessing import Pool
 
+usage = ('usage: bench.py [-c <count>] <.lox script>')
 
-time = 0
 count = 100000
+
+script = None
+argv = sys.argv[1:]
+while argv:
+    match argv[0]:
+        case '-c':
+            argv = argv[1:]
+            if not argv:
+                print(usage)
+                raise SystemExit(1)
+            count = int(argv[0])
+            argv = argv[1:]
+            continue
+    if script:
+        print('only one script is allowed')
+        print(usage)
+        raise SystemExit(1)
+    script = argv[0]
+    argv = argv[1:]
+
+if script is None:
+    print('usage: bench.py -c <count> <.lox script>')
+    raise SystemExit(1)
+
 
 
 def bench(_):
-    _, stderr = subprocess.Popen(['./zig-out/bin/zlox', 'fib_opt.lox'],
+    _, stderr = subprocess.Popen(['./zig-out/bin/zlox', script],
                                  stderr=subprocess.PIPE).communicate()
     return float(stderr.split()[1])
 
